2d5195ec497938c63209ba59a160428944e92c7e
BY <devblossom77@gmail.com>
2021-04-18 22:34:11 +0900

    Updates

---
+++ precache-manifest.8d12caaa21f5123d4607636cf8ba2f55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88c8af340b011d4c72257b11bb158e8",
+    "url": "/copy-unsplash/index.html"
+  },
+  {
+    "revision": "8dbcf12a155ec51ca216",
+    "url": "/copy-unsplash/static/css/main.90790d41.chunk.css"
+  },
+  {
+    "revision": "9a74d4070e50b229158d",
+    "url": "/copy-unsplash/static/js/2.138d2c24.chunk.js"
+  },
+  {
+    "revision": "8dbcf12a155ec51ca216",
+    "url": "/copy-unsplash/static/js/main.3c7b6f85.chunk.js"
+  },
+  {
+    "revision": "a1d38d6de04b67e3c0dd",
+    "url": "/copy-unsplash/static/js/runtime-main.776ccb67.js"
+  }
+]);
